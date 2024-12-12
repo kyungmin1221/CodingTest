7f2ceaa7d5a33adfd4c1dbe1ca66c1efc19967f8
KYUNGMIN PARK <105621255+kyungmin1221@users.noreply.github.com>
2024-12-12 20:33:45 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1224 ms, Memory: 174768 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            int query = Integer.parseInt(st.nextToken());
+            bw.write(map.getOrDefault(query, 0) + " ");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
